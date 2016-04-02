338fe4fa1afe88882cfb30d62071ecae02c9316a
Grace Egbo <grace.egbo1@gmail.com>
2016-04-02 12:11:05 -0500

    Completed lab

--- javacs-lab02/src/com/flatironschool/javacs/MyArrayList.java
+++ javacs-lab02/src/com/flatironschool/javacs/MyArrayList.java
@@ -63,6 +63,14 @@ public class MyArrayList<E> implements List<E> {
 			throw new IndexOutOfBoundsException();
 		}
 		// TODO: fill in the rest of this method
+		add(element);
+
+		for(int i=size-1; i>index; i--)
+		{
+			array[i] = array[i-1];
+		}
+
+		array[index] = element; 
 	}
 
 	@Override
@@ -112,7 +120,14 @@ public class MyArrayList<E> implements List<E> {
 	@Override
 	public int indexOf(Object target) {
 		// TODO: fill in this method
-		return 0;
+		for (int i = 0; i<size; i++)
+		{
+			if(equals(target,array[i]))
+			{
+				return i;
+			}
+		}
+		return -1;
 	}
 
 	/** Checks whether an element of the array is the target.
@@ -183,7 +198,12 @@ public class MyArrayList<E> implements List<E> {
 	@Override
 	public E remove(int index) {
 		// TODO: fill in this method.
-		return null;
+		E element = get(index);
+		for(int i = index; i<size-1;i++) {
+			array[i] = array[i+1];
+		}
+		size--;
+		return element;
 	}
 
 	@Override
@@ -203,7 +223,9 @@ public class MyArrayList<E> implements List<E> {
 	@Override
 	public E set(int index, E element) {
 		// TODO: fill in this method.
-		return null;
+		E old = get(index);
+		array[index] = element; 
+		return old; 
 	}
 
 	@Override
